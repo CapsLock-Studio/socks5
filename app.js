2272f157fff4eab2c473c728cde47b3cd440b68b
Michael Kuan <michael34435@capslock.tw>
2020-09-24 11:22:05 +0800

    feat: check http ping in 5000ms

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const compress = require('koa-compress');
 const helmet = require('koa-helmet');
 const axios = require('axios');
 const reachable = require('is-reachable');
+const SocksAgent = require('axios-socks5-agent')
 
 const app = new Koa();
 
@@ -13,7 +14,20 @@ app
     const data = await axios.get('https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt');
     const socks5 = data.data.split('\n').filter(Boolean);
     const stats = await Promise.all(socks5.map(reachable));
+    const servers = await Promise.all(
+      socks5
+        .filter((sock, index) => stats[index])
+        .map((server) => {
+          const [host, port] = server.split(':');
+          const { httpAgent, httpsAgent } = new SocksAgent({ host, port });
 
-    ctx.body = socks5.filter((sock, index) => stats[index]).join('\n');
+          return axios
+            .get('https://www.google.com', { httpAgent, httpsAgent, timeout: 5000 })
+            .then(() => server)
+            .catch(() => false);
+        }),
+    );
+
+    ctx.body = servers.filter(Boolean).join('\n');
   })
   .listen(3000);
