1690058a72c3addf96f861bbe6d91b88a77ae1f1
Michael Kuan <michael34435@capslock.tw>
2020-09-24 14:21:54 +0800

    feat: add other source and check proxy stats with google.com

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const _ = require('lodash');
 const Koa = require('koa');
 const compress = require('koa-compress');
 const helmet = require('koa-helmet');
 const axios = require('axios');
 const reachable = require('is-reachable');
-const SocksAgent = require('axios-socks5-agent')
+const SocksAgent = require('axios-socks5-agent');
 
 const app = new Koa();
 
@@ -11,18 +12,38 @@ app
   .use(compress())
   .use(helmet())
   .use(async (ctx) => {
-    const data = await axios.get('https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt');
-    const socks5 = data.data.split('\n').filter(Boolean);
+    const data = await Promise.all(
+      [
+        axios.get('https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt'),
+        axios.get('https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt'),
+        axios.get('https://www.proxyscan.io/download?type=socks5'),
+      ],
+    );
+    const socks5 = _
+      .chain(data)
+      .flattenDeep()
+      .map('data')
+      .join('\n')
+      .split('\n')
+      .filter()
+      .uniq()
+      .value();
     const stats = await Promise.all(socks5.map(reachable));
+    console.log(stats.filter(Boolean).length)
     const servers = await Promise.all(
       socks5
         .filter((sock, index) => stats[index])
         .map((server) => {
           const [host, port] = server.split(':');
           const { httpAgent, httpsAgent } = new SocksAgent({ host, port });
+          const options = {
+            httpAgent,
+            httpsAgent,
+            timeout: 3000,
+          }
 
           return axios
-            .get('https://www.google.com', { httpAgent, httpsAgent, timeout: 5000 })
+            .get('https://www.google.com', options)
             .then(() => server)
             .catch(() => false);
         }),
